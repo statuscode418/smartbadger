4e5c9880a5ab4ee2bbc672edd703a002a242bb1a
Aaron O. Ellis <aaronoellis@gmail.com>
2018-02-17 18:06:51 -0700

    Contracts detail API with balance

--- server/contracts/serializers.py
+++ server/contracts/serializers.py
@@ -2,17 +2,11 @@ from rest_framework import serializers
 
 
 class Contract(object):
-    def __init__(self):
-        self.address = "0x06012c8cf97bead5deae237070f9587f8e7a266d"
-        self.name = "Example Contract"
-        self.transaction_count = 3
-        self.block_number = 123346
-        self.eth_balance = 5.6576
+    def __init__(self, address, eth_balance):
+        self.address = address
+        self.eth_balance = eth_balance
 
 
 class ContractSerializer(serializers.Serializer):
-    address = serializers.CharField() # TODO byte field?
-    name = serializers.CharField()
-    transaction_count = serializers.IntegerField()
-    block_number = serializers.IntegerField()
+    address = serializers.CharField()
     eth_balance = serializers.FloatField()
--- server/contracts/views.py
+++ server/contracts/views.py
@@ -1,21 +1,29 @@
+from django.conf import settings
 from django.shortcuts import get_object_or_404
 from eth_utils import is_address
 from rest_framework import viewsets
 from rest_framework.exceptions import ValidationError
+from rest_framework.renderers import JSONRenderer
 from rest_framework.response import Response
+import web3
 
 from .serializers import Contract, ContractSerializer
 
 
 class ContractViewSet(viewsets.ViewSet):
     """List and get contracts"""
+    renderer_classes = (JSONRenderer, )
+
     def list(self, request):
-        data = [Contract()]
+        data = []
         serializer = ContractSerializer(data, many=True)
         return Response(serializer.data)
 
     def retrieve(self, request, pk=None):
         if not is_address(pk):
             raise ValidationError("Address must be a 20 byte hexidecimal or byte string, with or without prefix and padding")
-        serializer = ContractSerializer(Contract())
+
+        provider = web3.Web3(web3.HTTPProvider(settings.PROVIDER_URL))
+        balance = float(web3.Web3.fromWei(provider.eth.getBalance(pk), 'ether'))
+        serializer = ContractSerializer(Contract(pk, balance))
         return Response(serializer.data)
